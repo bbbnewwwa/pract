df26f64011a04a10da1d52eee29f564bf002e4eb
poc <johndoe@example.com>
2026-01-20 17:47:45 +0700

    2

--- beauty_studio/mysite/settings.py
+++ beauty_studio/mysite/settings.py
@@ -8,4 +8,19 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
-   
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    'services',
+    'appointments',
+    'users',
+
+    'rest_framework',
+]
+
+MIDDLEWARE = []
+
