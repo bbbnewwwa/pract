da338e36365837d1422e4079476ff87e80c0be2d
poc <johndoe@example.com>
2026-01-20 18:14:38 +0700

    СЕТТИНГС1

---
+++ beauty_studio/mysite/settings.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'мой сикретный ключииик'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = []
