a4c0985fc2150f033acc743350686c76c1f68afe
poc <johndoe@example.com>
2026-01-20 18:23:47 +0700

    СЕТТИНГС2

--- beauty_studio/mysite/settings.py
+++ beauty_studio/mysite/settings.py
@@ -7,4 +7,16 @@ SECRET_KEY = 'мой сикретный ключииик'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
-INSTALLED_APPS = []
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'services',
+    'appointments',
+    'users',
+
+    'rest_framework',]
