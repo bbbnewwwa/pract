ad0f85a3694feb0328838ed5a3587bed27031028
poc <johndoe@example.com>
2026-01-22 18:37:56 +0700

    сеттингс

--- beauty_studio/mysite/settings.py
+++ beauty_studio/mysite/settings.py
@@ -9,3 +9,9 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-...')
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
