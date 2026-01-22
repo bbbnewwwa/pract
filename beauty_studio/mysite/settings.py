edc42cda959cb1823e2cfbab76f6335939a3b971
poc <johndoe@example.com>
2026-01-22 18:46:30 +0700

    сеттингс я хочу uмиreть

--- beauty_studio/mysite/settings.py
+++ beauty_studio/mysite/settings.py
@@ -37,5 +37,13 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates'],  # шаблоны будут в frontend, но подключим позже
         'APP_DIRS': True,
-    }
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
