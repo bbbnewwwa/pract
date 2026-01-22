5ce6e7fd0b9b4198059e62b0893a1213df875b41
poc <johndoe@example.com>
2026-01-22 20:27:10 +0700

    жесть

--- apps/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Service
-
-@admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'duration_minutes', 'is_active')
-    list_editable = ('price', 'is_active')
-    list_filter = ('is_active',)
-    search_fields = ('name',)
