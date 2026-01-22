4a77195830f3a56b8a1dad8ffdde00a81b9294f7
poc <johndoe@example.com>
2026-01-22 19:56:50 +0700

    моделс

---
+++ apps/core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Service(models.Model):
+    name = models.CharField('Название услуги', max_length=100)
+    description = models.TextField('Описание', blank=True)
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    duration_minutes = models.PositiveIntegerField('Длительность (мин)', default=30)
+    image = models.ImageField('Изображение', upload_to='services/', blank=True, null=True)
+    is_active = models.BooleanField('Активна', default=True)
