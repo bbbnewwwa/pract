3a50b7bd26f0e435c1f674a5461742f4a8dd4c40
poc <johndoe@example.com>
2026-01-22 19:22:00 +0700

    сеттингс

--- beauty_studio/mysite/settings.py
+++ beauty_studio/mysite/settings.py
@@ -56,3 +56,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
