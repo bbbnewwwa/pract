47fdff3defad5a03a6abd250ef99bf64fd9818af
poc <johndoe@example.com>
2026-01-22 18:49:29 +0700

    сеттингс

--- beauty_studio/mysite/settings.py
+++ beauty_studio/mysite/settings.py
@@ -47,3 +47,12 @@ TEMPLATES = [
         },
     },
 ]
+
+WSGI_APPLICATION = 'beauty_studio.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
