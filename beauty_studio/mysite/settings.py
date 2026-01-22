badcd6e92bb38ea4a9cd47f3c65a7bce63e3d2ed
poc <johndoe@example.com>
2026-01-22 18:37:27 +0700

    сеттингс

--- beauty_studio/mysite/settings.py
+++ beauty_studio/mysite/settings.py
@@ -1,3 +1,11 @@
 import os
 from pathlib import Path
 from decouple import config
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-...')
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
