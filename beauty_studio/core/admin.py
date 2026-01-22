da1152032c7288af8045d869f1d81b60398e1731
poc <johndoe@example.com>
2026-01-22 21:49:26 +0700

    админ

--- beauty_studio/core/admin.py
+++ beauty_studio/core/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
 from .models import Client, Master, Service, Appointment, Order, Review, Promotion
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'phone')
+
+@admin.register(Master)
+class MasterAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'specialization', 'commission_percent')
